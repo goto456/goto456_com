9d92133f1018cecc2869c55d514c3f6e16a40f42
aven <wangbiwen_88@qq.com>
2016-01-12 19:08:15 +0800

    modify some code file.

--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,18 @@
 
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.http import Http404
 from models import Article
 
 # Create your views here.
 def home(request):
     post_list = Article.objects.all()
     return render_to_response('home.html', {'post_list': post_list})
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id = int(id))
+    except Article.DoesNotExit:
+        raise Http404
+    return render_to_response('post.html', {'post': post})
 
